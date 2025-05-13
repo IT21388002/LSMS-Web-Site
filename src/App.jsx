d77b7bd586e5c6f2a3b06302df8bdd82da2f8f7c
M.A, Pawara Hasamal <it21388002@my.sliit.lk>
2025-05-13 16:57:22 +0530

    Complete Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
 
 import './App.css'
+import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 
 function App() {
 
 
   return (
-    <>
-      <Navbar />
-    </>
+    // <main className='relative min-h-screen overflow-x-hidden'>
+    //   <div className='absolute -top-28 -left-28 w-[900px] h-[900px] bg-gradient-to-tr from-indigo-500/20 to-pink-500/20 rounded-full blur-[80px] -z-10'>
+    //   </div>
+    //   <div className='overflow-hidden'>
+    //     <Navbar />
+    //     <Hero />
+    //   </div>
+    // </main>
+
+    <main className='relative min-h-screen overflow-x-hidden'>
+      {/* Left Gradient Blob */}
+      <div className='absolute -top-28 -left-28 w-[900px] h-[900px] bg-gradient-to-tr from-indigo-500/20 to-pink-500/20 rounded-full blur-[80px] -z-10'></div>
+
+      {/* Right Gradient Blob */}
+      <div className='absolute -top-28 -right-28 w-[900px] h-[900px] bg-gradient-to-tr from-pink-500/20 to-indigo-500/20 rounded-full blur-[80px] -z-10'></div>
+
+      <div className='overflow-hidden'>
+        <Navbar />
+        <Hero />
+      </div>
+    </main>
+
   )
 }
 
