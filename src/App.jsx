3a900ef0f3950a6bbeb9fc4194e50367086c10ba
M.A, Pawara Hasamal <it21388002@my.sliit.lk>
2025-05-13 11:45:42 +0530

    Complete Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
 
 
   return (
     <>
-      <h1 className=''></h1>
+      <Navbar />
     </>
   )
 }
